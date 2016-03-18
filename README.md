adde3577167182e4d75e11110c6b92e8b84bc743
js201208030313 <hslee@wisol.co.kr>
2016-03-18 10:53:57 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Not real project but test purpose only
+
+## SlideSwitchRect
+- To use on FlashLight app. 
+- Move class to hsLib project after verification is done.
+### Layer
+4.title text  
+3-2.key knob  
+3-1.key  
+2.progress bar  
+1.slide bar  
+0.base  
---
+++ src/com/example/andengineheesangtestonoffice/SlideSwitchRect.java
@@ -0,0 +1,14 @@
+package com.example.andengineheesangtestonoffice;
+
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+public class SlideSwitchRect extends Rectangle {
+
+	public SlideSwitchRect(float pX, float pY, float pWidth, float pHeight,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pWidth, pHeight, pVertexBufferObjectManager);
+		// TODO Auto-generated constructor stub
+	}
+
+}
